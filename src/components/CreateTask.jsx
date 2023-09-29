1d15b5ddb4e5ad5c10e20863cbe49c7f0e0bec06
Pedro Paulo da Silva Monteiro <pedropaulopyter6@gmail.com>
2023-09-29 11:16:12 -0300

    Create task File done

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -7,89 +7,89 @@ import styles from '../styles/createtask.module.css'
 const API = "http://localhost:5000"
 
 function CreateTask() {
-  const [title, setTitle] = useState("");
-  const [time, setTime] = useState("");
-  const [todos, setTodos] = useState([]);
-  const [loading, setLoading] = useState(false);
+    const [title, setTitle] = useState("");
+    const [time, setTime] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [loading, setLoading] = useState(false);
 
 
-  useEffect(() => {
+    useEffect(() => {
 
-    const loadData = async () => {
+        const loadData = async () => {
 
-      setLoading(true);
+            setLoading(true);
 
-      const res = await fetch(API + "/todos")
-        .then((res) => res.json())
-        .then((data) => data)
-        .catch((err) => console.log(err));
+            const res = await fetch(API + "/todos")
+                .then((res) => res.json())
+                .then((data) => data)
+                .catch((err) => console.log(err));
 
-      setLoading(false);
-      setTodos(res);
+            setLoading(false);
+            setTodos(res);
+
+
+        };
+        loadData();
+    }, [])
+
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const todo = {
+            id: Math.random(),
+            title,
+            time,
+            done: false,
+
+        }
+        await fetch(API + "/todos", {
+            method: "POST",
+            body: JSON.stringify(todo),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        setTodos((prevState) => [...prevState, todo]);
+
+
+
+        console.log(todo)
+        setTime("");
+        setTitle("");
+
+    };
+
+    const handleDelete = async (id) => {
+
+        await fetch(API + "/todos/" + id, {
+            method: "DELETE",
+        });
+        setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+        alert("Tarefa excluída com sucesso!");
+    };
+
+    const handleEdit = async (todo) => {
+
+        todo.done = !todo.done;
+
+        const data = await fetch(API + "/todos/" + todo.id, {
+            method: "PUT",
+            body: JSON.stringify(todo),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        setTodos((prevState) => prevState.map((t) =>
+            (t.id === (data.id) ? (t = data) : t)));
 
 
     };
-    loadData();
-  }, [])
 
-
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const todo = {
-      id: Math.random(),
-      title,
-      time,
-      done: false,
-
+    if (loading) {
+        return <p>Carregando...</p>;
     }
-    await fetch(API + "/todos", {
-      method: "POST",
-      body: JSON.stringify(todo),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    setTodos((prevState) => [...prevState, todo]);
-
-
-
-    console.log(todo)
-    setTime("");
-    setTitle("");
-
-  };
-
-  const handleDelete = async (id) => {
-
-    await fetch(API + "/todos/" + id, {
-      method: "DELETE",
-    });
-    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
-    alert("Tarefa excluída com sucesso!");
-  };
-
-  const handleEdit = async (todo) => {
-
-    todo.done = !todo.done;
-
-    const data = await fetch(API + "/todos/" + todo.id, {
-      method: "PUT",
-      body: JSON.stringify(todo),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    setTodos((prevState) => prevState.map((t) =>
-      (t.id === (data.id) ? (t = data) : t)));
-
-
-  };
-
-  if (loading) {
-    return <p>Carregando...</p>;
-  }
 
 
 
@@ -97,62 +97,48 @@ function CreateTask() {
 
 
 
-  return (
-    <main className={styles.mainContainer}> 
-    <MenuTask/>
-        <div className={styles.createTask_Container}>
-            <div>
-                <h1>Create Task</h1>
-            </div>
-          <form onSubmit={handleSubmit}>
-            <div className='form-control'>
-              <label htmlFor='title'>O que voce vai fazer?</label><br />
+    return (
+        <main className={styles.mainContainer}>
+            <MenuTask />
+            <div className={styles.createTask_Container}>
+                <div className={styles.createTask_Container_top}>
+                    <h1>Create Task</h1>
+                </div>
+                <div className={styles.createTask_Container_forms}>
+                    <form onSubmit={handleSubmit}>
+                        <div className={styles.forms_Control}>
+                            <label className={styles.forms_Control_Label} htmlFor='title'>O que voce vai fazer?</label><br />
 
-              <input
-                type='text' name="title"
-                placeholder='Titulo da tarefa'
-                onChange={(e) =>
-                  setTitle(e.target.value)}
-                value={title || ""}
-                required
-              />
+                            <input
+                            className={styles.forms_Input}  
+                                type='text' name="title"
+                                placeholder='Titulo da tarefa'
+                                onChange={(e) =>
+                                    setTitle(e.target.value)}
+                                value={title || ""}
+                                required
+                            />
+                        </div>
+
+                        <div className={styles.forms_Control}>
+                            <label className={styles.forms_Control_Label} htmlFor='time'>Duração :</label><br />
+                            <input
+                            className={styles.forms_Input}
+                                type='text' name="time"
+                                placeholder='Tempo estimado (em horas)'
+                                onChange={(e) =>
+                                    setTime(e.target.value)}
+                                value={time || ""}
+                                required
+                            />
+                        </div>
+                        <input className={styles.button} type="submit" value="Criar tarefa" />
+                    </form>
+                </div>
             </div>
 
-            <div className='form-control'>
-              <label htmlFor='time'>Duração :</label><br />
-              <input
-                type='text' name="time"
-                placeholder='Tempo estimado (em horas)'
-                onChange={(e) =>
-                  setTime(e.target.value)}
-                value={time || ""}
-                required
-              />
-            </div>
-            <input className='enviar' type="submit" value="Criar tarefa" />
-          </form>
-        </div>
-        <div className='list-todo'>
-          <h2 className='listtitle'>Lista de tarefas:</h2>
-
-          {todos.length === 0 && <p>Não há tarefas</p>}
-          {todos.map((todo) => (
-            <div className='todo' key={todo.id}>
-              <h3 className={todo.done ? "todo-dobe" : ""}>{todo.title}</h3>
-              <p>Duração:{todo.time}{todo.time <= 1 ? "hr" : "hrs"}</p>
-              <span onClick={() => handleEdit(todo)}>
-                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
-              </span>
-              <BsTrash onClick={() =>
-                handleDelete(todo.id)
-
-              } />
-            </div>
-          ))}
-        
-      </div>
-    </main>
-  );
+        </main>
+    );
 
 };
 export default CreateTask;
